73cc32ac4586cf5cd407749c9bc48278dc92dab9
bulb03 <gh2752@gmail.com>
2017-12-15 23:53:51 +0800

    Update scrap.py

--- scrap_104_success/scrap.py
+++ scrap_104_success/scrap.py
@@ -7,34 +7,43 @@ import requests
 from selenium import webdriver
 import time
 
-wd = webdriver.Firefox()
+wd = webdriver.Edge()
 
 url_list = []
 
-job = ["系統分析與設計工程師","專案經理","韌體工程師","電玩軟體程式工程師","MIS網管人員","MIS工程師","巨量資料分析師","資訊安全工程師","網路安全分析師","網路管理工程師","產品設備維修工程師","資訊及通訊操作技術人員","電腦網路及系統技術人員","網站技術人員","多媒體動畫設計人員","遊戲設計人員","網站多媒體程式開發人員","廣告行銷企劃人員","品牌企劃人員","網路行銷人員"]
+job = ["系統分析與設計工程師","專案經理","韌體工程師"]#,"電玩軟體程式工程師","MIS網管人員","MIS工程師","巨量資料分析師","資訊安全工程師","網路安全分析師","網路管理工程師","產品設備維修工程師","資訊及通訊操作技術人員","電腦網路及系統技術人員","網站技術人員","多媒體動畫設計人員","遊戲設計人員","網站多媒體程式開發人員","廣告行銷企劃人員","品牌企劃人員","網路行銷人員"]
 
 #各職業的專屬檔案
 file_name = []
 
+#顯示skill和else_skill的記憶體位址
+def show_memory_place(skill,else_skill):
+	print(str(id(skill)),str(id(else_skill)))
+	pass
+
+
+#先建立好檔案名稱
 def set_file_name(job,file_name):
 	count = 0
 	for i in job:
 		file_name.append("C://Users//apple//Desktop//test//1111//test2_"+str(count)+".txt")
+		count += 1
+	return file_name
 
+#將職稱寫進它專屬的檔案
 def make_file(file_name,work):
 	with open(file_name,"w") as textfile:
 		textfile.write(work)
 
+#爬下來的資料寫進檔案
 def write_into_file(file_name,count_data,skill,else_skill):
 	with open(file_name,"a") as textfile:
 		textfile.write(str(count_data))
-		textfile.write(skill)
-		textfile.write(else_skill)
+		textfile.write("skill:",str(skill))
+		textfile.write("else_skill:",str(else_skill))
 		textfile.write("-------------------------------------\n")
 
-def show_memory_place(skill,else_skill):
-	print(str(id(skill)),str(id(else_skill)))
-
+#取得各職稱底下的職缺URL
 def get_jobs_url():
 	url = "https://www.1111.com.tw/job-bank/job-index.asp?si=1&ss=s&ks="+work
 
@@ -50,7 +59,7 @@ def get_jobs_url():
 	try:
 		for i in data:
 			time.sleep(1)
-			for j in i.find('div',{'class':'jbInfo'}).find('div',{'class':'jbInfoin'}).find('h3').findAll('a',limit=3):
+			for j in i.find('div',{'class':'jbInfo'}).find('div',{'class':'jbInfoin'}).find('h3').findAll('a',limits=3):
 				url_list.append("https:"+j.attrs['href'])
 	except AttributeError as error:
 		print(error)
@@ -58,6 +67,7 @@ def get_jobs_url():
 
 	return url_list
 
+#取得職缺的所需技能
 def get_skill(i):
 	skill = " "
 	else_skill = " "
@@ -68,39 +78,37 @@ def get_skill(i):
 	the_page = BeautifulSoup(html,"html.parser")
 	try:
 		time.sleep(1)
-		data = the_page.find('div',{'id':'descriptin'}).find('section',{'id':'incontent'}).find('div',{'id':'c1'}).find('div',{'class':'floatL.w65'}).find('article',{'class':'boxsize'}).find('ul',{'class':'dataList'})
-		for i in data.findAll('li'):
-			if count < 6:
-					count += 1
-			elif count == 6:
-				time.sleep(1)
-				skill = i.find('div',{'class':'listContent'}).get_text()
-				skill += " \n"
-				count += 1
-			else:
-				for j in i.find('div',{'class':'listContent'}).next_sibling.findAll('p'):
-					time.sleep(1)
-					else_skill += j.text
-					else_skill += " \n"
-				count += 1
+		data = the_page.find('div',{'id':'descriptin'}).find('section',{'id':'incontent'}).find('div',{'id':'c1'}).find('div',{'class':'floatL w65'}).find('article',{'class':'boxsize'}).find('ul',{'class':'dataList'})
+		data_ = data.findAll('li')
+
+		if len(data_)==7:
+			#<p>抓不到
+			#print(data_[6])
+			skill = data_[5].find('div',{'class':'listContent'}).get_text()
+			time.sleep(1)
+			skill += " \n"
+			#ㄟ，姓顏的，你真的很可憐，蠢到沒藥醫，他媽<div class="listContent">的是一個<li>裡面只有一個而已，你原本是在next_sibling三小?			
+			else_skill = data_[6].find('div',{'class':'listContent'}).get_text()
+			else_skill += " \n"
+		elif len(data_)==5:
+			time.sleep(1)
+			#<p>抓不到
+			skill = data_[4].find('div',{'class':'listContent'}).get_text()
+			skill += " \n"
+		else:
+			pass
 	except AttributeError as error:
 		pass
 
-	print(skill)
-	print(else_skill)
-	print("-------------------------------------\n")
-
 	return skill,else_skill
 
+#取得檔案名稱
+file_name = set_file_name(job,file_name)
 
-	file_name = make_file(job)
-
-set_file_name(job,file_name)
+career_count = 0
 
 for work in job:
 
-	career_count = 0
-		
 	count_data = 1
 
 	#取得每個職業url
@@ -108,25 +116,17 @@ for work in job:
 
 	#抓每個工作的需求技能
 	for i in url_list:
+		#建立檔案
 		make_file(file_name[career_count],work)
 
 		#取得工作技能
 		skill,else_skill=get_skill(i)
-
 		print(skill)
 		print(else_skill)
-		show_memory_place(skill,else_skill)
-
+		print("---------------------\n")
 		try:
-			show_memory_place(skill,else_skill)
 			write_into_file(file_name[career_count],count_data,skill,else_skill)
-			show_memory_place(skill,else_skill)
 		except:
 			pass
 		count_data += 1
 	career_count+=1
-
-
-#sublime基礎熱鍵：https://blog.shinychang.net/2014/01/09/Sublime%20Text%20%E5%9F%BA%E7%A4%8E%E7%86%B1%E9%8D%B5%E8%A1%A8/
-
-
